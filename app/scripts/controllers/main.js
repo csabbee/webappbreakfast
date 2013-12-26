159b3a27a6c39ca112b468ab4c8b04738ae58d6d
Csaba Valyi <csaba.valy1@gmail.com>
2013-12-26 11:42:38 +0100

    init commit after yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('breakfastApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
