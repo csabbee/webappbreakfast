b2f671fafda6ebde5e1fd1b1190a6298b87558e6
Csaba Valyi <csaba.valy1@gmail.com>
2013-12-27 09:31:31 +0100

    generated code replaced

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('breakfastApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, foodcatalog) {
+    $scope.foodcatalog = foodcatalog;
   });
