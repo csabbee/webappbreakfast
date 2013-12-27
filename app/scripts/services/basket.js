e3c1be3b450091d7bd2c768b7919e0a2b8d90ed1
Csaba Valyi <csaba.valy1@gmail.com>
2013-12-27 20:56:12 +0100

    [bugfix] basket now works as is should

--- app/scripts/services/basket.js
+++ app/scripts/services/basket.js
@@ -3,15 +3,16 @@
 angular.module('breakfastApp')
     .factory('basket', function() {
 
-
       var basket = {};
       var count = 0;
+      var total = 0;
 
-      return{
+      return {
         get: function () {
           return basket;
         },
         add: function (item) {
+          console.log(count);
           if (basket[item.id]) {
             basket[item.id].quantity += 1;
           } else {
@@ -20,15 +21,19 @@ angular.module('breakfastApp')
               quantity: 1
             };
           }
+          total += item.price;
+          count += 1;
         },
         remove: function(item) {
-          if(basket[item.id]){
-            count -= basket[item.id].amount;
-            delete basket[item.id];
-          }
+          count -= basket[item.type.id].quantity;
+          total -= basket[item.type.id].quantity * item.type.price;
+          delete basket[item.type.id];
         },
         count: function() {
           return count;
+        },
+        total: function() {
+          return total;
         }
       };
     });
