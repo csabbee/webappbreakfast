dc4e7d3c3ad868020f2de4ae37d9412edc0f7260
Csaba Valyi <csaba.valy1@gmail.com>
2013-12-27 10:59:44 +0100

    injecting basket into main js

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 angular.module('breakfastApp')
-  .controller('MainCtrl', function ($scope, foodcatalog) {
+  .controller('MainCtrl', function ($scope, foodcatalog, basket) {
     $scope.foodcatalog = foodcatalog;
+    $scope.basket = basket;
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -16,7 +16,7 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should attach a list of foodcatalog to the scope', function () {
+    expect(scope.foodcatalog.length).toBe(3);
   });
 });
