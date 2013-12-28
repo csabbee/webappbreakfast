a74549c53c2a401e4f67bccfbfb8cf7cc095b3f2
Csaba Valyi <csaba.valy1@gmail.com>
2013-12-28 17:48:48 +0100

    reducing quantity function testing

--- test/spec/services/basket.js
+++ test/spec/services/basket.js
@@ -46,4 +46,31 @@ describe('Service: Basket', function () {
     // THEN
     expect(Basket.count()).toBe(3);
   });
+
+  it('should reduce the quantity of the item in the basket', function(){
+    // GIVEN: adding items to the basket
+    foodcatalog.forEach(function(data){
+      Basket.add(data);
+      Basket.add(data);
+    });
+    expect(Basket.count()).toBe(6);
+    // WHEN: reduce items quantity by 1
+    foodcatalog.forEach(function(data){
+      Basket.decrement(data);
+    });
+    // THEN
+    expect(Basket.count()).toBe(3);
+  });
+
+  it('should remove item from the basket when the quantity is 0', function(){
+    // GIVEN: adding items to the basket
+    foodcatalog.forEach(function(data){
+      Basket.add(data);
+    });
+    expect(Basket.count()).toBe(3);
+    // WHEN: reduce one item's quantity to 0
+    Basket.decrement(foodcatalog[0]);
+    // THEN
+    expect(Basket.count()).toBe(2);
+  });
 });
