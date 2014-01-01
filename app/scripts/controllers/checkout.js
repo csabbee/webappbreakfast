7dc2a7a1d8a0b1aff64ef8b7cdfc5613c219b96a
Csaba Valyi <csaba.valy1@gmail.com>
2014-01-01 10:34:47 +0100

    firebase integrated into the app

--- app/scripts/controllers/checkout.js
+++ app/scripts/controllers/checkout.js
@@ -1,6 +1,24 @@
 'use strict';
 
 angular.module('breakfastApp')
-  .controller('CheckoutCtrl', function ($scope, basket) {
+  .controller('CheckoutCtrl', function ($scope, basket, $firebase) {
+    /*global Firebase */
+    var firebaseref = new Firebase('https://csabivalyi.firebaseio.com/basket');
+    var listItem = {};
     $scope.basket = basket;
+    $scope.firebasket = $firebase(firebaseref);
+
+    $scope.refresh = function() {
+      console.log('refresh called');
+      //getting every item from the basket
+      angular.forEach($scope.basket.get(), function(data){
+        listItem = {
+          type: data.type,
+          quantity: data.quantity
+        };
+        $scope.firebasket.$add(listItem);
+        listItem = {};
+      });
+    };
+
   });
